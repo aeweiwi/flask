9ad61b344f40fae8d58163c843942ebd9ab22866
abdalrahman <abdalrahman.eweiwi@gmail.com>
2018-06-27 23:15:55 +0200

    login finished

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from app import db, login
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model, UserMixin):
@@ -14,6 +15,17 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return '<User {}'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,7 +37,5 @@ class Post(db.Model):
         return '<Post {}>'.format(self.body)
 
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+
 
