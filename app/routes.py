1530ec7f042a50333bced96488a34014104343fe
abdalrahman <abdalrahman.eweiwi@gmail.com>
2018-07-05 01:14:57 +0200

    started pagination

--- app/routes.py
+++ app/routes.py
@@ -2,19 +2,30 @@ from app import app, db
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
-from app.forms import LoginForm, RegistrationForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.models import User, Post
+from datetime import datetime
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index',  methods=['GET', 'POST'])
 @login_required
 def index():
+
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+
     user = {'username': 'abdalrahman'}
     posts = [
         {'author': {'username':'abd1'},
          'body':'alright, alright, alright!'}
     ]
-    return render_template('index.html', title='HOME', posts=posts)
+
+    return render_template('index.html', title='HOME', posts=posts, form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -71,3 +82,52 @@ def user(username):
              {'author': user, 'body': 'Test post #2'}]
     return render_template('user.html', user=user, posts=posts)
 
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me  = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
+
+
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found '.format(username))
+        return redirect(url_for('idnex'))
+    if user == current_user:
+        flash("You can't follow yourself")
+        return redirect(url_for('index'))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are now following {}!'.format(username))
+    return redirect(url_for('user', username=username))
+
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('user {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash("can't unfollow yourself")
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are not following{}.'.format(username))
+    return redirect(url_for('user', username=username))
--- config.py
+++ config.py
@@ -6,3 +6,9 @@ class Config(object):
     SECRET_KEY='you will never get me'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+    #MAIL_SERVER =''
+    #MAIL_PROT =''
+    #MAIL_USE_TLS =''
+    #MAIL_USERNAME =''
+    #MAIL_PASSWORD = ''
+    #ADMINS = ['email address']
